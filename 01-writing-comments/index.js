996c7be892bb5066c403ead7534acfd9db3346fb
jovankaa77 <git@stackblitz.com>
2025-02-22 21:31:50 +0700

    Initial commit

---
+++ 01-writing-comments/index.js
@@ -0,0 +1,6 @@
+// 1. Username : Jovanka_Alexandro
+/* 2. Komentar banyak : 
+Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer.
+*/
---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-js-dbs
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jovankaa77/stackblitz-js-dbs)
